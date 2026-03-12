18d711221f20f7a8c76e8d8db8ebefab6afaf493
Abhishek Tripathi <abhishek.tripathi1@reckitt.com>
2026-03-12 17:06:35 +0530

    deploy docker image

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -2,7 +2,7 @@ express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World! \nWelcome to the node app running on AWS EC2 instance! \nThis is a simple Node.js application deployed on AWS EC2 using Terraform and GitHub Actions.');
+  res.send('123456432Hello World! Welcome to the node app running on AWS EC2 instance! This is a simple Node.js application deployed on AWS EC2 using Terraform and GitHub Actions.');
 });
 
 app.listen(8080, function () {
